02b54cdbfb9e38c81ac278f2bd68ac912e1e08a4
[TatianaHolubenko] <tatiana.holubenko01@gmail.com>
2024-02-10 21:09:00 +0200

    transaction

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import css from "./TransactionHistory.module.css";
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.head}>
+        <tr className={css.line}>
+          <th className={css.column}>Type</th>
+          <th className={css.column}>Amount</th>
+          <th className={css.column}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.tableColumn} key={id}>
+            <td className={css.transaction}>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
